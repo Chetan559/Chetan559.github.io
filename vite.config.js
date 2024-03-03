e0074915d5d7d2854fd4f32e5ef14fb2e332d3dd
Chetan Sharma <cschetan559@gmail.com>
2024-03-03 11:49:42 +0530

    Deployment(1)

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/CHETAN559.GITHUB.IO/",
+  base: "/Chetan559.github.io/",
 });
